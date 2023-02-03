bbe977aebd16a4d7d1196e3f5935dd2b69712947
Gadjetiny <brucetube12@gmail.com>
2023-02-03 21:29:53 +0300

    we started working with arrays

--- Arrayproject.cpp
+++ Arrayproject.cpp
@@ -3,5 +3,15 @@ using namespace std;
 
 int main()
 {
+    const int SIZE = 10;
+    short mas[SIZE];
+    short i;
+    for (i = 0; i < 10; i++) {
+        mas[i] = i + 1;
+        cout << mas[i] << ' ';
+
+
+    }
+        
     
 }
